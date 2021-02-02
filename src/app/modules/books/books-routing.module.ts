5e8fb8f2960f7a0c05e6b65b08f94305125f743d
Walter Ramos <juramosd@gmail.com>
2021-02-01 21:03:05 -0500

    angular pages modules

--- src/app/modules/books/books-routing.module.ts
+++ src/app/modules/books/books-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookDetailPageComponent } from './pages/book-detail-page/book-detail-page.component';
+import { BookListPageComponent } from './pages/book-list-page/book-list-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BookListPageComponent
+  },{
+    path: ':id',
+    component: BookDetailPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/books/books.module.ts
+++ src/app/modules/books/books.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BooksRoutingModule } from './books-routing.module';
+import { BookListPageComponent } from './pages/book-list-page/book-list-page.component';
+import { BookDetailPageComponent } from './pages/book-detail-page/book-detail-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BookListPageComponent, BookDetailPageComponent],
   imports: [
     CommonModule,
     BooksRoutingModule
